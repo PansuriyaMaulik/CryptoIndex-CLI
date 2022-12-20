7a137f3e89fe0b5e7ad4e9fc89196efef15d663d
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-12-20 15:43:56 +0100

    Build KeyManager funcion for set, get and delete the key

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,27 @@
+const inquirer = require('inquirer');
+const Colors = require('colors');
+const KeyManager = require('../lib/KeyManager.js');
+
 const key = {
-    set() {
-        console.log("Hello from set");
+    async set() {
+        //create instance of KeyManager
+        const keyManager =  new KeyManager();
+
+        //Take the Api key from user
+        const input = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'key',
+                message: 'Enter APi Key '.green + 'https://www.coinapi.io'
+            }
+        ]);
+
+        //Set the API Key
+        const key = keyManager.setKey(input.key);
+
+        if(key) {
+            console.log('API key set successfully'.blue);
+        }
     },
     show() {
         console.log("Hello from show");
