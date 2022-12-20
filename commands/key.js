4164e24ef2bccca4b385526e299d481856ff30bd
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-12-20 15:57:26 +0100

    Develop two method show and remove to validate API key

--- commands/key.js
+++ commands/key.js
@@ -25,11 +25,31 @@ const key = {
             console.log('API key set successfully'.blue);
         }
     },
+
     show() {
-        
+        try {
+        //create instance of KeyManager
+        const keyManager =  new KeyManager();
+        const key = keyManager.getKey();
+
+        console.log('Current Api key is: ', key.yellow);
+        return key;
+        } catch (e) {
+            console.error(e.message.red);
+        }
     },
+
     remove() {
-        console.log("Hello from remove");
+       try {
+        //create instance of KeyManager
+        const keyManager =  new KeyManager();
+        keyManager.deleteKey();
+
+        console.log('Key is removed'.blue);
+        return;
+        } catch (e) {
+            console.error(e.message.red);
+        }
     }
 }
 
